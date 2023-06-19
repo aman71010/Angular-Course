53386555110da4c2ad53b8be910c6f07461d85b6
Aman Gupta <67543598+aman71010@users.noreply.github.com>
2023-06-19 12:12:57 +0530

    replace EventEmitter with subject

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,4 +1,5 @@
-import { EventEmitter, Injectable } from "@angular/core";
+import { Injectable } from "@angular/core";
+
 import { Recipe } from "./recipe.model";
 import { Ingredient } from "../shared/ingredients.model";
 import { ShoppingListService } from "../shopping-list/shopping-list.sevice";
@@ -43,8 +44,6 @@ export class RecipeService{
             ]
         ),
     ];
-
-    //recipeSelected = new EventEmitter<Recipe>();
 
     constructor(private shoppingListService: ShoppingListService){}
 
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+
 import { Ingredient } from '../shared/ingredients.model';
 import { ShoppingListService } from './shopping-list.sevice';
 
@@ -7,8 +9,9 @@ import { ShoppingListService } from './shopping-list.sevice';
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
-export class ShoppingListComponent implements OnInit{
+export class ShoppingListComponent implements OnInit, OnDestroy{
   ingredients: Ingredient[];
+  private subscription: Subscription;
 
   constructor(private shoppingListService: ShoppingListService){}
 
@@ -17,5 +20,9 @@ export class ShoppingListComponent implements OnInit{
     this.shoppingListService.ingredientsChanged.subscribe( (ingredients: Ingredient[]) => {
       this.ingredients = ingredients;
     });
+  }
+
+  ngOnDestroy(){
+    this.subscription.unsubscribe();
   }
 }
