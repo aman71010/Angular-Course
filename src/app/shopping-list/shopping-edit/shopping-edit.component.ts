8257dd9b71cf04b42160d9b3d1290f4ebbcd768f
Aman Gupta <67543598+aman71010@users.noreply.github.com>
2023-06-22 14:38:07 +0530

    changed form in shopping-edit component(uses template-driven approach)

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -8,14 +8,12 @@ import { ShoppingListService } from '../shopping-list.sevice';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent {
-  @ViewChild('textInput') textInputRef: ElementRef; 
-  @ViewChild('amountInput') amountInputRef: ElementRef;
 
   constructor(private shoppingListService: ShoppingListService){}
 
-  onAddItem(){
-    const ingName = this.textInputRef.nativeElement.value;
-    const ingAmount = this.amountInputRef.nativeElement.value;
+  onAddItem(form: HTMLFormElement){
+    const ingName = form.value.name;
+    const ingAmount = form.value.amount;
     const newIngredient = new Ingredient(ingName, ingAmount);
     this.shoppingListService.addIngredient(newIngredient);
   }
