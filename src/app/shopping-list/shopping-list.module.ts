7712bc58664f32cc8c9c3324ff3027a3c00149ee
Aman Gupta <67543598+aman71010@users.noreply.github.com>
2023-07-04 17:02:00 +0530

    added shoppingListModule

---
+++ src/app/shopping-list/shopping-list.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { CommonModule } from "@angular/common";
+import { RouterModule } from "@angular/router";
+
+import { ShoppingEditComponent } from "./shopping-edit/shopping-edit.component";
+import { ShoppingListComponent } from "./shopping-list.component";
+
+@NgModule({
+    declarations: [
+        ShoppingListComponent,
+        ShoppingEditComponent,
+    ],
+    imports: [
+        FormsModule,
+        CommonModule,
+        RouterModule.forChild([{path: 'shopping-list', component: ShoppingListComponent}])
+    ]
+})
+export class ShoppingListModule {
+
+}
